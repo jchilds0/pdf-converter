3ef76852a4a62383d00d5a74be91e2875f61361c
Josh <joshua.childs01@outlook.com>
2024-11-20 00:28:53 +1100

    font size for text wrapping

--- app/Text.hs
+++ app/Text.hs
@@ -2,9 +2,8 @@ module Text (module Text) where
 import Markdown (MDTree (Document), Blocks, Leaf (Heading), Block (Leaf, Paragraph, Quote, Code, ListItem), Marker)
 import PDF (PDFTree, pdfCreateCatalog, pdfCreatePageTree, pdfCreatePage, Page, Object (Indirect, Inline), Position (Point), pdfCreateTextObject, Text (Text), Dictionary, IndirectType (Dictionary), InlineType (Name))
 import FreeType (ft_With_FreeType, ft_Load_Char, FT_FaceRec (frGlyph), ft_With_Face, FT_GlyphSlotRec (gsrAdvance), FT_Vector (FT_Vector), ft_Set_Char_Size)
-import Foreign (Storable(peek))
+import Foreign (Storable(peek), Int64)
 import Data.Char (ord)
-import Debug.Trace (trace)
 
 margin :: Int
 margin = 72 
@@ -15,7 +14,7 @@ width = 595
 height :: Int
 height = 842
 
-paragraphFontSize :: Int
+paragraphFontSize :: Int64
 paragraphFontSize = 12
 
 codeFontSize :: Int
@@ -38,7 +37,7 @@ paragraphFont = [
 rowPadding :: Int
 rowPadding = 2
 
-headingScale :: Int
+headingScale :: Int64
 headingScale = 2
 
 start :: Position
@@ -73,15 +72,15 @@ blockToObject (ListItem m b) pos = listItemToObject m b pos
 leafToObjects :: Leaf -> Position -> ([Object], Position)
 leafToObjects (Heading n s) pos = ([obj], newPos)
     where 
-        fontSize = paragraphFontSize + (7 - n) * headingScale
-        (obj, newPos) = textObject s fontSize pos
+        size = paragraphFontSize + (7 - fromIntegral n) * headingScale
+        (obj, newPos) = textObject s (fromIntegral size) pos
 leafToObjects _ (Point x y) = ([], Point x y)
 
 paragraphToObjects :: [String] -> Position -> ([Object], Position)
 paragraphToObjects [] pos = ([], pos)
 paragraphToObjects (l:ls) pos1 = (obj:objs, pos3)
     where 
-        (obj, pos2) = textObject l paragraphFontSize pos1
+        (obj, pos2) = textObject l (fromIntegral paragraphFontSize) pos1
         (objs, pos3) = paragraphToObjects ls pos2
 
 codeToObjects :: [String] -> Position -> ([Object], Position)
@@ -113,8 +112,8 @@ fontPath = "./fonts/CourierPrime-Regular.ttf"
 
 wrapBlock :: Block -> IO Blocks
 wrapBlock (Paragraph text) = do
-    let paraWidth = width - margin
-    ls <- wrapText paraWidth "" text
+    let paraWidth = width - 2 * margin
+    ls <- wrapText paragraphFontSize paraWidth "" text
     let blocks = map Paragraph ls
     return blocks
 wrapBlock block = return [block]
@@ -124,25 +123,25 @@ addLine line ss = do
     ls <- ss
     return (line:ls)
 
-wrapText :: Int -> String -> String -> IO [String]
-wrapText _ line "" = return [line]
-wrapText w line text = do 
+wrapText :: Int64 -> Int -> String -> String -> IO [String]
+wrapText _ _ line "" = return [line]
+wrapText fontSize w line text = do 
     let currentLine = if null line then "" else line ++ " "
     let (c1, r1) = splitSpace currentLine text
-    nextCharWidth <- mapM charWidth c1
+    nextCharWidth <- mapM (charWidth fontSize) c1
     let nextLineWidth = sum nextCharWidth
 
-    if nextLineWidth <= w * 72 then wrapText w c1 r1 else addLine line (wrapText w "" text)
+    if nextLineWidth <= w * 72 then wrapText fontSize w c1 r1 else addLine line (wrapText fontSize w "" text)
 
 splitSpace :: String -> String -> (String, String) 
 splitSpace current "" = (current, "")
 splitSpace current (' ':remaining) = (current, remaining)
 splitSpace current (c:remaining) = splitSpace (current ++ [c]) remaining
 
-charWidth :: Char -> IO Int
-charWidth c = ft_With_FreeType $ \lib -> 
+charWidth :: Int64 -> Char -> IO Int
+charWidth fontSize c = ft_With_FreeType $ \lib -> 
     ft_With_Face lib fontPath 0 $ \face -> do
-        ft_Set_Char_Size face 0 (16 * 64) 0 0
+        ft_Set_Char_Size face 0 (fontSize * 64) 0 0
         ft_Load_Char face (fromIntegral $ ord c) 4
         slot <- peek . frGlyph =<< peek face
         let (FT_Vector vX _) = gsrAdvance slot
